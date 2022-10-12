afff1dd3a77f1eca6eabfd14987a2fe1c7e3f104
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-10-12 19:50:39 +0300

    Removed Dependencies

--- index.js
+++ index.js
@@ -18,3 +18,22 @@ app.get('/', async (request, response) => {
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`App Link http://localhost:3000`))
+
+/*
+Database
+
+
+
+
+INSERT INTO PERSON
+(name, email, dob, country)
+VALUES ('Ahmed', 'ahmedl@gmail.com' ,  TO_DATE('03/09/1999', 'DD/MM/YYYY'), 'Bahrain');
+
+
+INSERT INTO PERSON
+(name, email, dob, country)
+VALUES ('Yousif', 'yousif@gmail.com' ,  TO_DATE('04/05/1999', 'DD/MM/YYYY'), 'Bahrain');
+
+
+
+*/
